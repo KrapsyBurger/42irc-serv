eb2cc532385d8d4d3a517615bc1e6f6293f59ebf
Nathan FASCIA <nfascia@e2r3p13.clusters.42paris.fr>
2023-02-23 16:13:32 +0100

    coucou

--- srcs/test.cpp
+++ srcs/test.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <unistd.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main()
 {
-	std::cout << "coucou" << std::endl;
+	int sockfd = socket(PF_LOCAL, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		std::cerr << "An error has occured while creating the socket." << std::endl;
+		return (-1);
+	}
+	std::cout << "socket is : " << sockfd << std::endl;
+	
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = PF_LOCAL;
+	serverAddr.sin_port = htons(8080);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	int	connect_result = connect(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if (connect_result < 0)
+	{
+		std::cerr << "Erreur lors de la connexion au socket" << std::endl;
+		return (-1);
+	}
+	std::cout << "Le client est connecte sur le port 8080." << std::endl;
 	return (0);
 }
