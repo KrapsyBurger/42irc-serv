23e42fff66163b66f3c83c9d9ba0d89a5712c4fa
Nathan FASCIA <nfascia@e2r3p13.clusters.42paris.fr>
2023-02-23 16:39:48 +0100

    coucou

--- srcs/test.cpp
+++ srcs/test.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-	int sockfd = socket(PF_LOCAL, SOCK_STREAM, 0);
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 	{
 		std::cerr << "An error has occured while creating the socket." << std::endl;
@@ -16,16 +16,25 @@ int main()
 	std::cout << "socket is : " << sockfd << std::endl;
 	
 	struct sockaddr_in serverAddr;
-	serverAddr.sin_family = PF_LOCAL;
+	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(8080);
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	int	connect_result = connect(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if (connect_result < 0)
+	int	bind_result = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if (bind_result < 0)
 	{
-		std::cerr << "Erreur lors de la connexion au socket" << std::endl;
+		std::cerr << "Error : cannot bind to the socket." << std::endl;
 		return (-1);
 	}
+
+
+	int listen_result = listen(sockfd, 5);
+	if (listen_result < 0)
+	{
+		std::cerr << "Error : cannot listen on this port." << std::endl;
+		return (-1);
+	}
+
 	std::cout << "Le client est connecte sur le port 8080." << std::endl;
 	return (0);
 }
